32b7df91d0e0d278e0825a82f9deedc14e356a55
Maciej Spólnik <56538884+maciekspolnik@users.noreply.github.com>
2021-01-26 16:02:15 +0100

    clearing repository

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-const USERNAME = 'jwkbqwarzstznw';
-const PASSWORD = '8f7aeb4fabb5cd204d5194f9491992772a4729deb24050f7662b17fd99ba82ef';
-const HOST ='ec2-79-125-77-37.eu-west-1.compute.amazonaws.com
-';
-const DATABASE = 'ddudn5oslmn7hl';
